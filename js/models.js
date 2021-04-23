24b7d79d848f7b109b8d7d4094990ad5f01c8390
Mitzi17 <m_estrada17@live.com>
2021-04-22 21:27:00 -0700

    defined static all to fetch all items

--- js/models.js
+++ js/models.js
@@ -9,4 +9,23 @@ class Item {
         let whitelist = ["name", "sku", "price", "location", "sizes", "category"]
         whitelist.forEach(attr => this[attr] = attributes[attr])
     }
+
+    static all() {
+        return fetch("http://localhost:3000/items", {
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            }
+        })
+        .then(res => {
+            if (res.ok) {
+                return res.json()
+            } else {
+                return res.text().then(error => Promise.reject(error))
+            }
+        })
+        .then(itemsArray => {
+            debugger
+        })
+    }
 }
